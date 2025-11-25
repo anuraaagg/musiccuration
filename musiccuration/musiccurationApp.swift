6de98aeaae41eafe9f62d75a60ac8973cb2f36d5
anuraaagg <s.anurag1998@gmail.com>
2025-11-25 17:08:44 +0530

    Initial Commit

---
+++ musiccuration/musiccurationApp.swift
@@ -0,0 +1,17 @@
+//
+//  musiccurationApp.swift
+//  musiccuration
+//
+//  Created by Anurag Singh on 25/11/25.
+//
+
+import SwiftUI
+
+@main
+struct musiccurationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
